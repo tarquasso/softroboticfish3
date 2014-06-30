e18735810e4bd2b9fd51590b5202b73a21022d3d
Yanni Coroneos <ycoroneo@mit.edu>
2014-06-30 20:33:38 +0000

    abandoned adafruit for pyBBIO

--- PwmIn.py
+++ PwmIn.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #This class can read the pwm value on a GPIO pin
-import Adafruit_BBIO.GPIO as GPIO
+from bbio import *
 import time #for timer functionality
-import threading, os, sys
+import os, sys
 
 class PwmIn:
 	def __init__(self, pin, minDuty, maxDuty):
@@ -12,23 +12,26 @@ class PwmIn:
 		self.period=0
 		self.pulsewidth=0
 		self.risen=False
-		GPIO.setup(self.pin, GPIO.IN)
-		self.trise=threading.Thread(target=self.rise)
-		self.tfall=threading.Thread(target=self.fall)
+		pinMode(self.pin, INPUT)
+		try:
+			attachInterrupt(self.pin, self.rise, RISING)
+			attachInterrupt(self.pin, self.fall, FALLING)
+		except RuntimeError:
+			detachInterrupt(self.pin)
+			delay(4000)
+			attachInterrupt(self.pin, self.rise, RISING)
+			attachInterrupt(self.pin, self.fall, FALLING)
 		self.time=time.clock() #start the timer
-		self.trise.start()
-		self.fall.start()
 
 	def __del__(self):
+		pass
 		#cleanup
-		self.trise.join()
-		self.tfall.join()
+		detachInterrupt(self.pin)
 
 	def getPeriod(self):
 		return self.period
 
 	def rise(self):
-		GPIO.wait_for_edge(self.pin, GPIO.RISING) #wait for the rising edge
 		_tmp=time.clock()
 		diff=_tmp-self.time #look at time difference
 		if (diff>=0.019): #rc receiver has 19ms period
@@ -37,7 +40,6 @@ class PwmIn:
 			self.time=time.clock() #reset timer
 
 	def fall(self):
-		GPIO.wait_for_edge(self.pin, GPIO.FALLING)
 		_tmp=time.clock()
 		diff=_tmp-self.time()
 		if (diff>5e-4 and dif<0.005 and self.risen==True):
@@ -47,8 +49,12 @@ class PwmIn:
 
 #for debugging
 if(__name__ == "__main__"):
-	PwmIn channel("P8_14", 0, 100)
-	print "reading 100 times..."
-	for i in xrange(100):
-		print str(channel.getPeriod())
+	channel = PwmIn(GPIO1_28, 0, 100)
+	def setup():
+		print "reading 100 times..."
+		for i in xrange(100):
+			print str(channel.getPeriod())
+	def loop():
+		sys.exit(0)
+	run(setup,loop)
 
