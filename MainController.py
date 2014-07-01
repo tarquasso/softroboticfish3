a14b5a69b4046955928ab812c531b101734bccf4
Yanni Coroneos <ycoroneo@mit.edu>
2014-07-01 15:47:07 +0000

    ctrl-c works now

--- MainController.py
+++ MainController.py
@@ -6,17 +6,19 @@ import Syren, Fishgait
 
 class MainController:
 	def __init__(self):
+		bbio_init()
 		self.syren=Syren.Syren(Serial1, 9600) #init syren
 		self.syren.update(50) #zero it for now
 		self.leftservo=PWM2A
 		self.rightservo=PWM2B
-		self.gait=FishGait.SawGait(freq, amp)
+		self.gait=Fishgait.SawGait(3, 1)
 	
 	def cleanup(self):
 		self.syren.finish()
+		bbio_cleanup()
 
 	def control(self):
 		duty=self.gait.compute() #get the current duty cycle
 		self.syren.update(duty) #update syren
-		pwmWrite(self.leftservo, 50)
-		pwmWrite(self.rightservo, 50)
+#		analogWrite(self.leftservo, 50)
+#		analogWrite(self.rightservo, 50)
--- main.py
+++ main.py
@@ -3,15 +3,21 @@
 
 import os, sys, signal
 import MainController
+from bbio import *
 
 if (__name__=="__main__"):
 	running=True
 	def signal_handler(signal, frame):
+		print "got sigint"
+		global running
 		running=False
 	
 	signal.signal(signal.SIGINT, signal_handler)
 	mainCtrl=MainController.MainController()
+	print "running. press ctrl-c to kill"
 	while (running==True):
 		mainCtrl.control()
+		delay(10)
+	print "quitting"
 	mainCtrl.cleanup()
 	sys.exit(0)
