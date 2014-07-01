de23eb9856ff7fcb86a169c0105a74352eb22507
Yanni Coroneos <ycoroneo@mit.edu>
2014-07-01 15:01:12 +0000

    must manually destruct syren. updated syren unit test

--- Syren.py
+++ Syren.py
@@ -7,16 +7,17 @@ class Syren:
 		self.port=port
 		self.port.begin(baud)
 
-	def __del__(self):
+	def finish(self):
 		self.port.flush()
 		self.port.end()
 	
 	def update(self, duty):
 		control=float(duty)*(2.54)
 		self.port.flush()
-		self.port.prints(ord(control))
+		self.port.prints(int(control))
 
 if (__name__=="__main__"):
+	bbio_init()
 	sy=Syren(Serial1, 9600)
 	sy.update(0)
 	time.sleep(1)
@@ -25,3 +26,5 @@ if (__name__=="__main__"):
 	sy.update(100)
 	time.sleep(1)
 	sy.update(50)
+	sy.finish()
+	bbio_cleanup()
