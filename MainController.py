679c894819335b71d6c76977ff8a5e49f96203ae
spock <spock@csail.mit.edu>
2014-07-22 10:21:39 -0400

    pictures and video work at 320x240

--- MainController.py
+++ MainController.py
@@ -9,7 +9,7 @@ import Syren, Fishgait
 
 class MainController:
 	def __init__(self):
-		bbio_init()
+		bbio.bbio_init()
 		self.syren=Syren.Syren(Serial1, 19200) #init syren
 		self.syren.update(50) #zero it for now
 		self.leftservo=PWM1A
@@ -21,18 +21,24 @@ class MainController:
 		self.cap=cv2.VideoCapture(0) #open default camera
 		if (self.cap.isOpened()==False):
 			self.cap=False #no camera :(
+		self.cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
+		self.cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
+		self.video=False #true if we're currently recording
 	
 	def cleanup(self):
 		if (self.cap):
 			self.cap.release()
 		self.syren.finish()
-		bbio_cleanup()
+		bbio.bbio_cleanup()
 
 	def control(self):
 		duty=self.gait.compute() #get the current duty cycle
 		self.syren.update(duty) #update syren
+		if (self.video != False):
+			self.video.write(self.cap.read()[1])
 		#analogWrite(self.leftservo, int(duty), RES_8BIT)
 		#analogWrite(self.rightservo, int(duty), RES_8BIT)
+	
 	def handle_input(self, keycode):
 		amount=0.01
 		keycode=int(keycode) #for safety
@@ -48,5 +54,13 @@ class MainController:
 		elif (keycode==ord('p')):
 			if (self.cap):
 				cv2.imwrite("pic-"+str(time.time())+".png", self.cap.read()[1])
+		elif (keycode==ord('v')):
+			if (self.cap):
+				if (self.video==False):
+					self.video=cv2.VideoWriter('output'+str(time.time())+'.avi', cv2.cv.CV_FOURCC('M','J','P','G'), 20.0, (320,240))
+					if (not self.video.isOpened()):
+						self.video=False
+				else:
+					self.video=False
 	def __str__(self):
 		return 'frq:%f amp:%f' % (self.gait.get_freq(), self.gait.get_amp())
