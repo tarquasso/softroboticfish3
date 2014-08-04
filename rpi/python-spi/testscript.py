aadf9c85b38b0613084060139bcac535fc72cafd
Robert <robert@katzschmann.de>
2014-08-04 21:38:25 +0000

    changes to testscript in spi

--- rpi/python-spi/testscript.py
+++ rpi/python-spi/testscript.py
@@ -1,21 +1,20 @@
 import spidev
 import time
-spi = spidev.SpiDev()
-spi.open(0, 1)
+spi = spidev.SpiDev()   # create spi object
+spi.open(0, 0)          # open spi port 0, device CE0 (CS 0)
 try:
     while True:
-        resp = spi.xfer2([0xAA])
-        print resp[0]
-        time.sleep(1)
+        resp = spi.xfer2([0xAA])    # transfer one byte, will send an array of bytes keeping the CE asserted the whole time.
+        #resp = spi.xfer([0xAA])   # will send an array of bytes de-asserting and re-asserting the CE with every byte.
+        print resp[0]               # print response 0:
+        time.sleep(1)               # sleep for 0.1 seconds
     #end while
-except KeyboardInterrupt:
-    # create spi object
-    # open spi port 0, device (CS) 1
-    # transfer one byte
-    # sleep for 0.1 seconds
-    # Ctrl+C pressed, so...
-    spi.close() # ... close the port before exit #end try
-#This script will reverse the bit ordering in one byte (if you are not able to change LSB / MSB first to your needs.
+except KeyboardInterrupt:           # Ctrl+C pressed, so...
+    spi.close() # ... close the port before exit 
+#end try
+
+#This additional script will reverse the bit ordering 
+#in one byte (if you are not able to change LSB / MSB first to your needs.
 def ReverseBits(byte):
     byte = ((byte & 0xF0) >> 4) | ((byte & 0x0F) << 4) 
     byte = ((byte & 0xCC) >> 2) | ((byte & 0x33) << 2) 
