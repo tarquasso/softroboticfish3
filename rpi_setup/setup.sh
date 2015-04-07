e8d124dada9dc028fd981fc7310079fd656b2d7b
Kris <kfrey@mit.edu>
2015-04-07 20:43:55 +0000

    Uncommented some lines in rpi_setup/setup.sh

--- rpi_setup/setup.sh
+++ rpi_setup/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 ##a script to set up the raspi
-# y | sudo apt-get update
-# y | sudo apt-get upgrade
-# y | sudo apt-get install python-picamera python-serial git
-# cd /tmp
-# git clone https://github.com/lurch/rpi-serial-console.git
-# cd rpi-serial*
-#  sudo rpi-serial-console disable
-# cd ..
-# rm -rf rpi-serial*
-# cd ~
+y | sudo apt-get update
+y | sudo apt-get upgrade
+y | sudo apt-get install python-picamera python-serial git
+cd /tmp
+git clone https://github.com/lurch/rpi-serial-console.git
+cd rpi-serial*
+sudo rpi-serial-console disable
+cd ..
+rm -rf rpi-serial*
+cd ~
 ##git clone https://github.com/ycoroneos/fishbrainz.git
 
 # Configure Edimax wifi dongle
