fd7d4227baf860d28b10e5dc8d58c1c42a9772f2
Kris Frey <kfrey@mit.edu>
2015-04-08 19:01:46 -0400

    Added reconstruct call in vis_servo.cpp

--- src/vis_servo.cpp
+++ src/vis_servo.cpp
@@ -22,30 +22,9 @@ int main(int argc, char** argv)
 	Mat centroids(2, K, CV_32S);
 	Mat colors(1, K, CV_UC3);
 	Mat labels(img.total(), 1, CV_8U);
-	get_centroids(&img, 6, centroids, colors, lables)
+	get_centroids(&img, 6, centroids, colors, labels);
 
-	// Normalize and draw circles at centroids
-	for (int k=0; k < K; k++)
-	{
-		Point center;
-		int num_samples = colorCentroids.at<int32_t>(2,k);
-		center.y = colorCentroids.at<int32_t>(0,k) / (float) num_samples;
-		center.x = colorCentroids.at<int32_t>(1,k) / (float) num_samples;
-		
-		int thickness = 2;
-		float radius = s.width/64.0;
-		int linetype = 1;
-		Vec3b color_v = colorTable.at<Vec3b>(k);
-		uchar b = color_v[0];
-		uchar g = color_v[1];
-		uchar r = color_v[2];
-		Scalar color(b,g,r);
-		circle(r_img, center, radius, color, thickness, linetype);
-	}
-
-	namedWindow("reduced image", WINDOW_AUTOSIZE);
-	imshow("reduced image", r_img);
-	waitKey();
+	reconstruct(img.size(), centroids, colors, labels);
 
 	return 0;
 }
