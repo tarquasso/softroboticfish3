d4b58c8c7a877cba176d5a00b94f95812e0ff5ac
Kris Frey <kfrey@mit.edu>
2015-05-02 02:23:02 +0000

    Added modified fstab file to rpi_config

--- rpi_setup/setup.sh
+++ rpi_setup/setup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 ##a script to set up the raspi
-y | sudo apt-get update
-y | sudo apt-get upgrade
-y | sudo apt-get install python-picamera python-serial git
-cd /tmp
-git clone https://github.com/lurch/rpi-serial-console.git
-cd rpi-serial*
-sudo rpi-serial-console disable
-cd ..
-rm -rf rpi-serial*
-cd ~
+#y | sudo apt-get update
+#y | sudo apt-get upgrade
+#y | sudo apt-get install python-picamera python-serial git
+#cd /tmp
+#git clone https://github.com/lurch/rpi-serial-console.git
+#cd rpi-serial*
+#sudo rpi-serial-console disable
+#cd ..
+#rm -rf rpi-serial*
+#cd ~
 ##git clone https://github.com/ycoroneos/fishbrainz.git
 
 # Configure Edimax wifi dongle
 sudo apt-get install wpasupplicant
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant/
 sudo cp 8192cu.conf /etc/modprobe.d/
+sudo cp fstab	/etc/fstab
 
 # Configure fstab to mount mBed as usb drive
 sudo mkdir /media/mbed
