40fbd0cff486c84a45943921aa0fc7381dc0feec
Kris Frey <kfrey@mit.edu>
2015-05-22 17:05:29 -0400

    Fixed launch file

--- src/commander.h
+++ src/commander.h
@@ -1,17 +1,23 @@
 // commander.h
 
+#ifndef COMMANDER_H
+#define COMMANDER_H
+
 #include "fishcode/VisOffset.h"
 #include "fishcode/SetSwimMode.h"
 
-#define SWIM_MODE_COUNT         5
+#define SWIM_MODE_COUNT         6
 #define SWIM_MODE_IDLE          0 
 #define SWIM_MODE_BTTN_DRIVE    1
 #define SWIM_MODE_RC_MANUAL     2
 #define SWIM_MODE_VIS_SERVO     3
 #define SWIM_MODE_CIRCLE		4
+#define SWIM_MODE_TEST			5
 
 using namespace fishcode;
 
 void commandSwimMode(int mode);
 void cb_SetSwimModeRqst(const SetSwimMode::ConstPtr& msg);
 void cb_VisOffset(const VisOffset::ConstPtr& msg);
+
+#endif
